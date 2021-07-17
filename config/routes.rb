87306141ccca5327eb7732aba42c5e7ac0e85822
Richard <rdesilvey@gmail.com>
2021-07-16 21:26:38 -0600

    Implement endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
       scope module: :merchants do
         resources :merchants, controller: :get_all_merchants, only: :index
         resources :merchant, controller: :get_merchant, only: :show
+        resources :merchant, only: [] do
+          resources :items, controller: :get_merchant_items, only: :index
+        end
       end
     end
   end
