054b196e3d68855fb50d03bf099db1ec9091694b
Richard <rdesilvey@gmail.com>
2021-07-16 20:35:41 -0600

    Write tests

---
+++ spec/requests/merchants/get_all_merchants_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe 'Get Merchants API Endpoint' do
+  # Initialize the test data
+  before { 
+    40.times do
+      FactoryBot.create(:merchant)
+    end
+  }
+
+  describe 'GET /merchants' do
+    context 'when merchants page is not given as a param' do
+      before { get '/merchants' }
+
+      it 'returns status code 200' do
+        @first_merchant = Merchant.first
+
+        expect(response).to have_http_status(200)
+        expect(json.length).to eq 20
+        expect(json.first['id']).to eq @first_merchant.id
+      end
+    end
+
+    context 'when merchants page is given' do
+      it 'returns status code 200' do
+        get '/merchants?page=1'
+        @first_merchant = Merchant.first
+
+        expect(response).to have_http_status(200)
+        expect(json.length).to eq 20
+        expect(json.first['id']).to eq @first_merchant.id
+      end
+
+      it 'returns next 20 merchants' do
+        get '/merchants?page=2'
+        @first_merchant = Merchant.first
+        
+        expect(response).to have_http_status(200)
+        expect(json.length).to eq 20
+        expect(json.first['id']).to_not eq @first_merchant.id
+      end
+    end
+  end
+end
