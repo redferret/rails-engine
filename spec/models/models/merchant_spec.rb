b9f4b0b49a7b08ebb028d1cc908a34759fd25453
Richard <rdesilvey@gmail.com>
2021-07-16 18:39:49 -0600

    Add tests

---
+++ spec/models/models/merchant_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Merchant, type: :model do
+  describe 'relationships' do
+    it { should have_many :items }
+  end
+
+  describe 'validations' do
+  end
+end
