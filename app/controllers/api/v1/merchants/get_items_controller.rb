f270d326592787a2693dff15051fabda3ad43ea0
Richard <rdesilvey@gmail.com>
2021-07-16 20:36:56 -0600

    Empty Controllers

---
+++ app/controllers/api/v1/merchants/get_items_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::Merchants::GetItemsController < Api::V1::ApplicationController
+  def index
+    
+  end
+end
---
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -0,0 +1,5 @@
+class Api::V1::Merchants::SearchController < Api::V1::ApplicationController
+  def show
+    
+  end
+end
