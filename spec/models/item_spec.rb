85ca1bf44a74fa486e277df169a72139572d1fba
Richard <rdesilvey@gmail.com>
2021-07-17 21:05:30 -0600

    Write tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -12,4 +12,20 @@ RSpec.describe Item, type: :model do
     it { should have_many(:invoices).through(:invoice_items) }
     it { should belong_to :merchant }
   end
+
+  describe 'class method,' do
+    context '::search_by_name' do
+      it 'searches for items by name case insensitive' do
+        merchant = create(:merchant)
+        item_1 = create(:item, name: 'Blarg', merchant: merchant)
+        item_2 = create(:item, name: 'Pillow', merchant: merchant)
+        item_3 = create(:item, name: 'Billowarg', merchant: merchant)
+        item_4 = create(:item, name: 'Staples', merchant: merchant)
+
+        expected = [item_1, item_3]
+        actual = Item.search_by_name('arG')
+        expect(actual).to eq expected
+      end
+    end
+  end
 end
