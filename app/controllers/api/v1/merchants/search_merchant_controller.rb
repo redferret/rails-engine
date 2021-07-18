234ccb8cbfceccfc26d874a303a3eea92d08ebca
Richard <rdesilvey@gmail.com>
2021-07-17 20:52:43 -0600

    Implement class method

--- app/controllers/api/v1/merchants/search_merchant_controller.rb
+++ app/controllers/api/v1/merchants/search_merchant_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::Merchants::SearchMerchantController < Api::V1::ApplicationController
   def show
     if params[:name].present? && !params[:name].empty?
-      @merchant = Merchant.find_by(name: params[:name])
+      @merchant = Merchant.search_by_name(params[:name])
 
       if @merchant
         render json: @merchant, status: :ok
       else
-        render json: Merchant.new, status: :ok
+        render json: {data: {}}, status: :ok
       end
     else
       render json: { messages: ['Illegal or missing search params given'] }, status: :bad_request
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,10 @@
 class Merchant < ApplicationRecord
+  validates :name, presence: true
+  
   has_many :items, dependent: :destroy
   has_many :invoice_items, through: :items
+
+  def self.search_by_name(name)
+    where('name Ilike ?', "%#{name}%").first
+  end
 end
