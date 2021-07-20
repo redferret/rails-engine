a08a3ad2aee6c84d8d6ed3c1b0fda14877e4fb39
Richard <rdesilvey@gmail.com>
2021-07-19 21:26:10 -0600

    Simplify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        resource :find, controller: :search_merchant, only: :show
+      end
       scope module: :merchants do
-        scope :merchants do
-          resource :find, controller: :search_merchant, only: :show
-        end
         resources :merchants, controller: :resources do
           resources :items, controller: :merchant_items, only: :index
         end
       end
+      namespace :items do
+        resources :find_all, controller: :search_items, only: :index
+      end
       scope module: :items do
-        scope :items do
-          resources :find_all, controller: :search_items, only: :index
-        end
         resources :items, only: [] do
           resource :merchant, controller: :item_merchant, only: :show
         end
