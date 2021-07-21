f96b3a389bf78823a776876b020b801745e55517
Richard <rdesilvey@gmail.com>
2021-07-21 13:56:55 -0600

    Update tests

--- spec/requests/merchants/revenue_spec.rb
+++ spec/requests/merchants/revenue_spec.rb
@@ -1,17 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchants by revenue' do
-  before :each do
-    create_list(:merchant, 30) do |merchant|
-      customer = create(:customer)
-      create_list(:item, 5, merchant: merchant) do |item|
-        invoice = create(:invoice, customer: customer, merchant: merchant)
-        create(:invoice_item, item: item, invoice: invoice)
+  describe 'GET /api/v1/revenue/merchants/:id' do
+    before :each do
+      create_list(:merchant, 5) do |merchant|
+        customer = create(:customer)
+        create_list(:item, 5, merchant: merchant) do |item|
+          invoice = create(:invoice, customer: customer, merchant: merchant)
+          create(:invoice_item, item: item, invoice: invoice)
+        end
+      end
+      @merchant = Merchant.all.sample
+    end
+
+    context 'gets the merchants revenue' do
+      it 'returns status 200 with the revenue data' do
+        get "/api/v1/revenue/merchants/#{@merchant.id}"
+        expect(response).to have_http_status 200
+
+        merchant = json_single
+
+        expect(merchant).to have_key(:id)
+        expect(merchant).to have_key(:type)
+        expect(merchant).to have_key(:attributes)
+
+        attributes = merchant[:attributes]
+
+        expect(attributes).to have_key(:revenue)
+      end
+    end
+
+    context 'merchant not found' do
+      it 'returns status 404' do
+        get "/api/v1/revenue/merchants/3248765"
+        expect(response).to have_http_status 404
+
+        error = errors
+        expect(error).to have_key(:error)
+        expect(error[:error]).to eq 'Resource not found'
       end
     end
   end
 
   describe 'GET /api/v1/revenue/merchants?quantity=' do
+    before :each do
+      create_list(:merchant, 30) do |merchant|
+        customer = create(:customer)
+        create_list(:item, 5, merchant: merchant) do |item|
+          invoice = create(:invoice, customer: customer, merchant: merchant)
+          create(:invoice_item, item: item, invoice: invoice)
+        end
+      end
+    end
+
     context 'gets 10 merchants' do
       before { get '/api/v1/revenue/merchants?quantity=10' }
 
