51218b23bfdc8c7719e1cf889896dcd9136d9abb
Richard <rdesilvey@gmail.com>
2021-07-20 23:09:22 -0600

    Lint code

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,7 +1,7 @@
 class Merchant < ApplicationRecord
   validates :name, presence: true
 
-  has_many :invoices
+  has_many :invoices, dependent: :destroy
   has_many :items, dependent: :destroy
   has_many :invoice_items, through: :items
 
@@ -10,10 +10,12 @@ class Merchant < ApplicationRecord
   end
 
   def self.total_revenue_by_descending_order
+    total_price = '(invoice_items.unit_price * invoice_items.quantity)'
+
     joins('inner join invoices on invoices.merchant_id = merchants.id')
-    .joins('inner join invoice_items on invoice_items.invoice_id = invoices.id')
-    .where(invoices: {status: :shipped})
-    .select('merchants.*, sum( round (cast (float8 (invoice_items.unit_price * invoice_items.quantity) as numeric), 2) ) as invoice_revenue')
-    .group('merchants.id').order('invoice_revenue desc')
+      .joins('inner join invoice_items on invoice_items.invoice_id = invoices.id')
+      .where(invoices: { status: :shipped })
+      .select("merchants.*, sum( round (cast (float8 #{total_price} as numeric), 2) ) as invoice_revenue")
+      .group('merchants.id').order('invoice_revenue desc')
   end
 end
