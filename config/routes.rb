a882dcbe1867095878f1335bdf0f189c1dd7820a
Richard <rdesilvey@gmail.com>
2021-07-21 15:01:35 -0600

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,12 +14,10 @@ Rails.application.routes.draw do
       namespace :items do
         resources :find_all, controller: :search_items, only: :index
       end
-      scope module: :items do
-        resources :items, only: [] do
-          resource :merchant, controller: :merchant, only: :show
-        end
-        resources :items, controller: :resources
+      resources :items, module: :items, only: [] do
+        resource :merchant, controller: :merchant, only: :show
       end
+      resources :items, controller: :resources, module: :items
     end
   end
 end
