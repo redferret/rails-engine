dd53fc823cf9fe757f282abf196335c57e65fae8
Richard <rdesilvey@gmail.com>
2021-07-21 15:03:19 -0600

    lint

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,24 @@ Rails.application.routes.draw do
       scope :revenue do
         resources :merchants, controller: :revenue, module: :merchants, only: [:index, :show]
       end
+
       namespace :merchants do
         resource :find, controller: :search_merchant, only: :show
         resources :most_items, only: :index
       end
+
       resources :merchants, controller: :resources, module: :merchants do
         resources :items, only: :index
       end
+
       namespace :items do
         resources :find_all, controller: :search_items, only: :index
       end
+
       resources :items, module: :items, only: [] do
         resource :merchant, controller: :merchant, only: :show
       end
+
       resources :items, controller: :resources, module: :items
     end
   end
