ac54077ddc16f36644180ba2ba9fa8c5705365ba
Richard <rdesilvey@gmail.com>
2021-07-20 23:02:52 -0600

    Implement class method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,6 +3,7 @@ class Invoice < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
   has_many :items, through: :invoice_items
   belongs_to :customer
+  belongs_to :merchant
 
-  enum status: { in_progress: 'in progress', cancelled: 'cancelled', completed: 'completed' }
+  enum status: { in_progress: 'in progress', cancelled: 'cancelled', shipped: 'shipped' }
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,10 +1,19 @@
 class Merchant < ApplicationRecord
   validates :name, presence: true
 
+  has_many :invoices
   has_many :items, dependent: :destroy
   has_many :invoice_items, through: :items
 
   def self.search_by_name(name)
     where('name Ilike ?', "%#{name}%").first
+  end
+
+  def self.total_revenue_by_descending_order
+    joins('inner join invoices on invoices.merchant_id = merchants.id')
+    .joins('inner join invoice_items on invoice_items.invoice_id = invoices.id')
+    .where(invoices: {status: :shipped})
+    .select('merchants.*, sum( round (cast (float8 (invoice_items.unit_price * invoice_items.quantity) as numeric), 2) ) as invoice_revenue')
+    .group('merchants.id').order('invoice_revenue desc')
   end
 end
