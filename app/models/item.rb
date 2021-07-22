e353dd0753f0058b397f28649e5c0736272711b2
Richard <rdesilvey@gmail.com>
2021-07-22 14:59:25 -0600

    Fix bug

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,14 @@ class Item < ApplicationRecord
   def self.search_by_name(name)
     where('name Ilike ?', "%#{name}%")
   end
+
+  def self.items_revenue_desc_order(quantity, page = 1)
+    total_price = '(invoice_items.unit_price * invoice_items.quantity)'
+    records = joins(:invoice_items, :invoices)
+               .where(invoices: { status: :shipped })
+               .select('items.*')
+               .select("sum( round (cast (float8 #{total_price} as numeric), 2) ) as revenue")
+               .group(:id).order(revenue: :desc)
+    paginate(page, quantity, records)
+  end
 end
